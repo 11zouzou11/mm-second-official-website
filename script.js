9398a4a2fbf56fd996d2717f7dfdc19d38a181f6
11zouzou11 <josephchahine@live.com>
2023-07-19 08:41:21 +0000

    added styling

--- script.js
+++ script.js
@@ -18,3 +18,29 @@ $(document).ready(function() {
     });
   });
   
+
+  // Scrolling banner functionality
+  function startScrolling() {
+    const container = $(".scrolling-banner-container");
+    const banner = $(".scrolling-banner");
+    const firstLogo = banner.children().first();
+
+    // Calculate the total width of the logos in the banner
+    const totalWidth = banner.children().length * (firstLogo.outerWidth() + 20); // 20px for padding
+
+    // Set the initial position of the banner
+    banner.css("width", totalWidth + "px").css("left", container.outerWidth() + "px");
+
+    // Function to animate the scrolling
+    function animateScrolling() {
+      banner.animate({ left: -totalWidth + "px" }, totalWidth * 30, "linear", function() {
+        banner.css("left", container.outerWidth() + "px");
+        animateScrolling();
+      });
+    }
+
+    animateScrolling();
+  }
+
+  startScrolling();
+});
